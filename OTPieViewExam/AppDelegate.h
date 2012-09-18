952609afb5d08e10a2b0e642c94db505a5c2dd40
Eric Yeh <lanter77@gmail.com>
2012-09-18 15:44:28 +0800

    1. 完成 OTPieView 圖型輸出功能。

--- OTPieViewExam/AppDelegate.h
+++ OTPieViewExam/AppDelegate.h
@@ -7,9 +7,32 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <OTUIKit/OTPieView.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    IBOutlet SpeedometerView *meterView;
+    NSMutableDictionary *presetOneValues, *presetTwoValues, *presetThreeValues;
+	
+    NSButton* presetButtonOne;
+    NSButton* presetButtonTwo;
+    NSButton* presetButtonThree;
+    IBOutlet NSTokenField *setTokenField;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, assign) IBOutlet NSButton* presetButtonOne;
+@property (nonatomic, assign) IBOutlet NSButton* presetButtonTwo;
+@property (nonatomic, assign) IBOutlet NSButton* presetButtonThree;
+@property (nonatomic, assign) IBOutlet NSTokenField *setTokenField;
 
+@property (nonatomic, strong) NSMutableDictionary *presetOneValues;
+@property (nonatomic, strong) NSMutableDictionary *presetTwoValues;
+@property (nonatomic, strong) NSMutableDictionary *presetThreeValues;
+
+- (IBAction)presetOne:(id)sender;
+- (IBAction)presetTwo:(id)sender;
+- (IBAction)presetThree:(id)sender;
+- (IBAction)valueSet:(id)sender;
+- (IBAction)speedSet:(id)sender;
 @end
